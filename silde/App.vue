c8d711929674837cc26668c5df80156a89dbde8c
bds <459114173@qq.com>
2021-08-09 17:59:04 +0800

    test

--- silde/App.vue
+++ silde/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Descripttion: 
+ * @version: v1.0
+ * @Author: baidongsheng
+ * @Date: 2021-08-09 17:53:06
+ * @LastEditors: baidongsheng
+ * @LastEditTime: 2021-08-09 17:55:11
+-->
 <script>
 	export default {
 		onLaunch: function () {
@@ -14,4 +22,5 @@
 
 <style>
 	/*每个页面公共css */
+	/*测试 */
 </style>
